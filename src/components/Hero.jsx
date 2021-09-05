44439a22349f09a6c95b631dfc7c0f394a2cdc64
Bawanthathilan <bawantharathnayaka@gmail.com>
2021-09-05 15:00:13 +0530

    hero styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Hero.css'
-import Logo from '../assets/logo.png'
+import Logo from '../assets/logo-light.png'
 
 function Hero() {
   return (
     <div>
-      <nav class='navbar sticky-top navbar-light bg-light'>
+      <nav class='navbar sticky-top navbar-dark bg-dark'>
         <a class='navbar-brand' href='#'>
           <img
             src={Logo}
@@ -20,7 +20,7 @@ function Hero() {
       <section class='hero'>
         <div class='container'>
           <div class='left-col'>
-            <p class='subhead'>Lumoz Studio</p>
+            <p class='subhead'>Lumoz Community</p>
             <h1>Explore Models made just for Lumoz</h1>
             <p className='subhead-lumoz'>
               Explore and Download thousands of 3D Models
@@ -28,7 +28,7 @@ function Hero() {
 
             <div class='hero-cta'>
               <a href='www.lumoz.studio' class='primary-cta'>
-                Try for free
+                Try Lumoz for free
               </a>
             </div>
           </div>
